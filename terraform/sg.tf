5a7103b9e3081d2bc605860113cc77c32d08a086
mayssa <60172570+mayssa-bm@users.noreply.github.com>
2021-09-18 19:43:52 +0100

    Create sg.tf

---
+++ terraform/sg.tf
@@ -0,0 +1,28 @@
+# Configure Security Group
+resource "aws_security_group" "Jenkins-SG" {
+  name = "Jenkins SG"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
